39ddf89ba400b68e7036bf497db9748148851a72
Benjamin Schug <benjaminschug@gmail.com>
2016-01-30 19:21:05 +0100

    why did it have to be snakes... i hate snakes...

---
+++ Assets/Scripts/Snake/SnakeFactory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SnakeFactory : SingletonMonoBehaviour<SnakeFactory>
+{
+	public SnakeSegment SegmentPrefab;
+
+	int SnakeSize { get { return BalanceValues.Instance.SnakeSize; } }
+	float InitialSnakeSpawnDelay { get { return BalanceValues.Instance.InitialSnakeSpawnDelay; } }
+
+	void Start() {
+		Snake snake = CreateSnake();
+		SnakeSegment previousSegment = null;
+		for (var i = 0; i < SnakeSize; i++) {
+			var segment = GameObject.Instantiate( SegmentPrefab ).GetComponent<SnakeSegment>();
+			segment.Leader = previousSegment;
+			segment.gameObject.SetActive( false );
+			segment.gameObject.layer = snake.gameObject.layer;
+			segment.transform.parent = snake.transform;
+			snake.Segments.Add( segment );
+			previousSegment = segment;
+		}
+		snake.StartCoroutine( snake.Spawn( InitialSnakeSpawnDelay ) );
+	}
+
+	Snake CreateSnake() {
+		var go = new GameObject();
+		go.name = "Snake";
+		go.AddComponent<Snake>();
+		go.transform.parent = this.transform;
+		go.layer = this.gameObject.layer;
+		return go.GetComponent<Snake>();
+	}
+}
---
+++ Assets/Scripts/Snake/SnakePath.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakePath : MonoBehaviour {
+	public Transform[] Waypoints;
+	public GameObject DigAtStart;
+	public GameObject DigAtEnd;
+
+	public Vector3 StartPosition { get { return Waypoints[0].position; } }
+
+	int DigAtStartCount = 0;
+	int DigAtEndCount = 0;
+
+	public void ShowDigAtStart() {
+		DigAtStartCount++;
+		DigAtStart.SetActive( true );
+	}
+
+	public void HideDigAtStart() {
+		DigAtStartCount--;
+		if (DigAtStartCount == 0) {
+			DigAtStart.SetActive( false );
+		}
+	}
+
+	public void ShowDigAtEnd () {
+		DigAtEndCount++;
+		DigAtEnd.SetActive( true );
+	}
+
+	public void HideDigAtEnd() {
+		DigAtEndCount--;
+		if (DigAtEndCount == 0) {
+			DigAtEnd.SetActive( false );
+		}
+	}
+}
