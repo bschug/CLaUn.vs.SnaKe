8822f066b75f51cc78709db6f6c6126eca086071
Norbert Haacks <nafna89@gmail.com>
2016-01-30 23:10:31 +0100

    Added Script for destroying particles when they are done playing

---
+++ Assets/Scripts/SelfdestructParticle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfdestructParticle : MonoBehaviour {
+
+	private ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+
+		ps = GetComponent<ParticleSystem>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (ps){
+		
+			if(!ps.IsAlive()){
+			
+				Destroy(gameObject);
+			
+			}
+		
+		}
+	
+	}
+}
