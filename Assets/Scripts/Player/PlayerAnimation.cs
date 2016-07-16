640030628a1afd1f24bcaa0a90da74854fdb21c9
Benjamin Schug <benjaminschug@gmail.com>
2016-07-16 02:49:47 +0200

    cleaned up dirty player animation system

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(PlayerHealth))]
+[RequireComponent(typeof(Player))]
+class PlayerAnimation : MonoBehaviour
+{
+	public GameObject DustParticle;
+
+	Animator Animator;
+	Player Player;
+	PlayerMovement PlayerMovement;
+	PlayerHealth PlayerHealth;
+
+	int ANIPROP_MovementX;
+	int ANIPROP_MovementY;
+	int ANIPROP_IsAlive;
+	int ANIPROP_IsStunned;
+
+	void Awake () {
+		Animator = GetComponent<Animator>();
+		Player = GetComponent<Player>();
+		PlayerMovement = GetComponent<PlayerMovement>();
+		PlayerHealth = GetComponent<PlayerHealth>();
+	}
+
+	void Update() {
+		// Dust particle is based on actual movement
+		DustParticle.SetActive( IsMovingQuickly );
+
+		// animation must be based on input, not actual velocity 
+		// because it should show where the clown is trying to move, 
+		// which is not necessarily where it's actually moving (e.g. on ice)
+		var movement = InputManager.Instance.GetMovement( ClownId );
+		Animator.SetFloat( "MovementX", movement.x );
+		Animator.SetFloat( "MovementY", movement.y );
+
+		Animator.SetBool( "IsAlive", PlayerHealth.IsAlive );
+		Animator.SetBool( "IsStunned", PlayerMovement.IsStunned );
+	}
+
+	bool IsMovingQuickly { get { return PlayerMovement.CurrentVelocity.sqrMagnitude > 0.02f; } }
+	ClownId ClownId { get { return Player.ClownId; } }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,137 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
-public enum AnimationStates
-{
-	Idle,
-	Walk_Left,
-	Walk_Right,
-	Walk_Front,
-	Walk_Back,
-	Stun,
-	Death,
-
-	Default
-}
 
 [RequireComponent(typeof(PlayerHealth))]
 public class PlayerMovement : MonoBehaviour
 {
-	public GameObject[] walkAnimations;
 	public ClownId ClownId;
-	public AnimationStates animStates;
-
-	public GameObject cloudParticle;
 
 	PlayerHealth PlayerHealth;
+	Rigidbody2D Rigidbody2D;
 
 	float StunDurationRemaining;
 	public bool IsStunned { get { return StunDurationRemaining > 0; } }
 
-	float Speed { get { return BalanceValues.Instance.PlayerSpeed; } }
+	public float MaxSpeed { get { return BalanceValues.Instance.PlayerSpeed; } }
+	public Vector2 CurrentVelocity;
 
 	void Awake() {
 		PlayerHealth = GetComponent<PlayerHealth>();
+		Rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
 	void FixedUpdate() {
 		StunDurationRemaining -= Time.deltaTime;
+
 		if (!IsStunned && Intro.Instance.TutorialComplete) {
-			var movement = InputManager.Instance.GetMovement( ClownId );
-			UpdatePosition( movement );
+			UpdateVelocity();
 		}
-		PlayerDirAndSetAnimationStates();
-		SetAnimation();
     }
 
-	void UpdatePosition (Vector2 movement) {
-		GetComponent<Rigidbody2D>().velocity = movement * Speed;
+	void UpdateVelocity() {
+		var movementInput = InputManager.Instance.GetMovement( ClownId );
+		CurrentVelocity = movementInput * MaxSpeed;
+		GetComponent<Rigidbody2D>().velocity = CurrentVelocity;
 	}
 
 	public void Stun() {
 		StunDurationRemaining = BalanceValues.Instance.StunDuration;
 	}
-
-	void PlayerDirAndSetAnimationStates() {
-		Vector2 playerDir = InputManager.Instance.GetMovement( ClownId );
-		
-		if (!PlayerHealth.IsAlive) {
-			animStates = AnimationStates.Death;
-		}
-		else if (IsStunned) {
-			animStates = AnimationStates.Stun;
-		}
-		else if( Mathf.Abs(playerDir.x) <= 0.1f && Mathf.Abs(playerDir.y) <= 0.1f) {
-			animStates = AnimationStates.Idle;
-        }
-		else if (playerDir.x <= 0 && playerDir.y == 0) {
-			//links
-			animStates = AnimationStates.Walk_Left;
-		}
-		else if (playerDir.x >= 0 && playerDir.y == 0) {
-			//rechts
-			animStates = AnimationStates.Walk_Right;
-		}
-		else if (playerDir.x == 0 && playerDir.y >= 0) {
-			//oben
-			animStates = AnimationStates.Walk_Back;
-		}
-		else if (playerDir.x == 0 && playerDir.y <= 0) {
-			//unten
-			animStates = AnimationStates.Walk_Front;
-		}
-	}
-
-	void SetAnimation() {
-
-
-		if(animStates == AnimationStates.Idle) {
-			walkAnimations[0].SetActive( true );
-			cloudParticle.SetActive( false );
-
-		}
-		else {
-			walkAnimations[0].SetActive( false );
-			cloudParticle.SetActive( true );
-		}
-		if (animStates == AnimationStates.Walk_Left) {
-			walkAnimations[1].SetActive( true );
-		}
-		else {
-			walkAnimations[1].SetActive( false );
-		}
-		if (animStates == AnimationStates.Walk_Right) {
-			walkAnimations[2].SetActive( true );
-		}
-		else {
-			walkAnimations[2].SetActive( false );
-		}
-		if (animStates == AnimationStates.Walk_Front) {
-			walkAnimations[3].SetActive( true );
-		}
-		else {
-			walkAnimations[3].SetActive( false );
-		}
-		if (animStates == AnimationStates.Walk_Back) {
-			walkAnimations[4].SetActive( true );
-		}
-		else {
-			walkAnimations[4].SetActive( false );
-		}
-		if (animStates == AnimationStates.Stun) {
-			walkAnimations[5].SetActive( true );
-		}
-		else {
-			walkAnimations[5].SetActive( false );
-		}
-		if (animStates == AnimationStates.Death) {
-			walkAnimations[6].SetActive( true );
-		}
-		else {
-			walkAnimations[6].SetActive( false );
-		}
-
-	}
-
-
 
 }
