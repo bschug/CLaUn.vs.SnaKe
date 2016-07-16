cd04d35383e4276875257993b0fb146d54bd7ee0
Benjamin Schug <benjaminschug@gmail.com>
2016-07-16 18:56:14 +0200

    better vis for snake paths

--- Assets/Scripts/Snake/SnakePath.cs
+++ Assets/Scripts/Snake/SnakePath.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class SnakePath : MonoBehaviour {
 	public Transform[] Waypoints;
@@ -7,6 +8,8 @@ public class SnakePath : MonoBehaviour {
 	public GameObject DigAtEnd;
 
 	public Vector3 StartPosition { get { return Waypoints[0].position; } }
+
+	public Color GizmoColor = new Color( 0, 0, 0, 0 );
 
 	int DigAtStartCount = 0;
 	int DigAtEndCount = 0;
@@ -33,5 +36,27 @@ public class SnakePath : MonoBehaviour {
 		if (DigAtEndCount == 0) {
 			DigAtEnd.SetActive( false );
 		}
+	}
+
+	void OnDrawGizmos() {
+		if (GizmoColor.a == 0) {
+			GizmoColor = Color.HSVToRGB( Random.Range( 0f, 1f ), 1, 1 );
+		}
+
+		Gizmos.color = GizmoColor;
+		DrawNodeGizmo( Waypoints[0] );
+		for (var i=1; i < Waypoints.Length; i++) {
+			DrawNodeGizmo( Waypoints[i] );
+			DrawLineGizmo( Waypoints[i-1], Waypoints[i] );
+		}
+		DrawNodeGizmo( Waypoints.Last() );
+	}
+
+	void DrawNodeGizmo(Transform waypoint) {
+		Gizmos.DrawSphere( waypoint.position, 0.1f );
+	}
+
+	void DrawLineGizmo(Transform a, Transform b) {
+		Gizmos.DrawLine( a.position, b.position );
 	}
 }
